aad2228cfab756fbbfb5bb9474103ee107de75fa
Allen Ryerson <aryerson9@gmail.com>
2020-11-16 21:23:02 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Magento-User-Import
-php tool to mass create administrative users for Magento
+ For when you need to create a ton of Magento 2 admin users, and you dont have a cheap intern around. 
+
+ ## Usage
+ Edit the two global variables in `UserImport.php` with the path to your CSV file and the password you would like to set for the new users.  
---
+++ UserImport.php
@@ -0,0 +1,56 @@
+<?php
+
+    // Global vars. Update to suit your needs
+    $CSVFile ='s4stagingusers.csv';
+    $LoginPassword = 'Change123Me!';
+
+    use Magento\Framework\App\Bootstrap;
+    require 'app/bootstrap.php';
+    $bootstrap = Bootstrap::create(BP, $_SERVER);
+    $objectManager = $bootstrap->getObjectManager();
+    $UserFactory = $objectManager->get('\Magento\User\Model\UserFactory');
+
+    $csvFile = fopen($CSVFile, 'r');
+
+    while (($line = fgetcsv($csvFile)) !== FALSE) {
+
+        // Array structure from csv
+            // 0 = Username
+            // 1 = Firstname
+            // 2 = Lastname
+            // 3 = Email
+            // 4 = Role
+
+            $Username = (string) $line[0];
+            $FirstName = (string) $line[1];
+            $LastName = (string) $line[2];
+            $Email = (string) $line[3];
+            $Role = (int) $line[4];
+
+        try{
+            $adminInfo = [
+            'username'         => $Username,
+            'firstname'        => $FirstName,
+            'lastname'         => $LastName,
+            'email'            => $Email,
+            'password'         => $LoginPassword,       
+            'interface_locale' => 'en_US',
+            'is_active'        => 1
+        ];
+
+        $userModel = $UserFactory->create();
+        $userModel->setData($adminInfo);
+        $userModel->setRoleId($Role);
+        $userModel->save(); 
+    
+        } catch (\Exception $ex) {
+            echo $ex->getMessage();
+        }    
+        
+        echo "$Username was sucessfully created! \n"
+
+    }
+
+    fclose($csvFile);
+
+?>
